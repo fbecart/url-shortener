f2a403e190a988db6d94cd4fbd4bf06788f97c01
Florent Bécart <florent.becart@gmail.com>
2016-08-16 11:57:04 +0200

    Add static URL shortener
    [Iron](https://github.com/iron/iron) was selected as the underlying Web framework.
    [Nickel](https://github.com/nickel-org/nickel.rs/) came tight in the
    competition, but seems less idiomatic and popular.
    
    For testing purpose, this implementations understands two paths: `/google` and `/rockets`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+extern crate iron;
+
+use std::collections::HashMap;
+
+use iron::prelude::*;
+use iron::modifiers::Redirect;
+use iron::{Handler, Url, status};
+
+struct UrlShortenerHandler {
+    shortened_urls: HashMap<String, Url>,
+}
+
+impl UrlShortenerHandler {
+    fn new() -> Self {
+        let mut static_routes = HashMap::new();
+
+        let google_url = "http://google.com";
+        static_routes.insert("google".to_string(), Url::parse(google_url).unwrap());
+
+        let rockets_url = "https://www.washingtonpost.com/graphics/business/rockets/";
+        static_routes.insert("rockets".to_string(), Url::parse(rockets_url).unwrap());
+
+        UrlShortenerHandler { shortened_urls: static_routes }
+    }
+}
+
+impl Handler for UrlShortenerHandler {
+    fn handle(&self, req: &mut Request) -> IronResult<Response> {
+        let request_path = &req.url.path().join("/");
+        match self.shortened_urls.get(request_path) {
+            Some(url) => Ok(Response::with((status::Found, Redirect(url.clone())))),
+            None => Ok(Response::with(status::NotFound)),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let handler = UrlShortenerHandler::new();
+    Iron::new(handler).http("localhost:3000").unwrap();
 }
